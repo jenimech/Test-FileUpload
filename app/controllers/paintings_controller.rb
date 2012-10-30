36c5461426fd4d9d1b7e8a997d4e57489d63ae11
Eugenia Chacon <jenimech2@mail.com>
2012-10-30 10:45:34 -0300

    Is working the multi-upload

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,5 +1,10 @@
 class PaintingsController < ApplicationController
 
   def index
+    @paintings = Painting.all
+  end
+
+  def create
+    @painting = Painting.create(params[:painting])
   end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,10 @@
 class Painting < ActiveRecord::Base
   attr_accessible :name, :image
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+  before_create :default_name
+
+  def default_name
+    self.name ||= File.basename(image_file_name, '.*').titleize if image.file?
+  end
 end
